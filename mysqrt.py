7d63ce96f38fde4688e3410aca824e3219aaf135
ayush@DESKTOP-OCORL43 <ayush@DESKTOP-OCORL43.>
2025-02-04 21:49:28 +0000

    Added debug feature

--- mysqrt.py
+++ mysqrt.py
@@ -1,17 +1,17 @@
-def sqrt2(x):
+def sqrt2(x, debug):
     s= 1.5	
-    x=2.0
     kmax= 15
     tolerance= 1.0e-14;
     for k in range(kmax):	
-        print(f"At iteration {k}, the value of s is: {s}")
+        if debug:
+            print(f"At iteration {k}, the value of s is: {s:20.15f}")
         s1= s
         s=0.5*( s+x/s)
         delta= abs(s-s1)
-        print(f"Error at iteration {k} is {delta}")
-        if(abs(delta)/s < tolerance):
+        if debug:
+            print(f"Error at iteration {k} is {delta}")
+        if(delta/abs(x) < tolerance):
             break 
+    if debug:
         print(f"The final value of s is {s}")
-    from math import sqrt
-    print(f"The machine epsilon is: ", sqrt(2)-s)
-	
+    return s
