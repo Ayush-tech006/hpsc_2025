75d3e9f1f63be0318765cea2929d7aa630ddad7d
ayush@DESKTOP-OCORL43 <ayush@DESKTOP-OCORL43.>
2025-02-04 22:35:55 +0000

    added 2 functions in test_case.py

--- test_case.py
+++ test_case.py
@@ -1,13 +1,26 @@
-from numpy import sqrt
-from mysqrt import sqrt2
-small= 1.0e-14
+def test_1():
+    from numpy import sqrt
+    from mysqrt import sqrt2
+    small= 1.0e-14
 
-xvalues= [0,2,10,100,0.0001,1e8]
-for x in xvalues:
-    s= sqrt2(x)
-    s_numpy= sqrt(x)
-    print(f"fro x= {x}, s={s} and s_numpy= {s_numpy}")
-    assert(s-s_numpy)<small,f"square root disagrees with numpy square root for x={x}"
+    xvalues= [0,2,10,100,0.0001,1e5]
+    for x in xvalues:
+        s= sqrt2(x)
+        s_numpy= sqrt(x)
+        print(f"fro x= {x}, s={s} and s_numpy= {s_numpy}")
+        assert(s-s_numpy)<small,f"square root disagrees with numpy square root for x={x}"
 
 # In terminal: python3 test_case.py
 # In general we use pytest to run tests rather than using "python3 test_case.py"
+
+def test_2():
+    from numpy import sqrt
+    from mysqrt import sqrt2
+    small= 1.0e-14
+
+    xvalues= [3,6,"5"]
+    for x in xvalues:
+        s= sqrt2(x)
+        s_numpy= sqrt(x)
+        print(f"fro x= {x}, s={s} and s_numpy= {s_numpy}")
+        assert(s-s_numpy)<small,f"square root disagrees with numpy square root for x={x}"
