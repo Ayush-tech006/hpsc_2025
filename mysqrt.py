0138f6f3f2df1a59002c3a2c6ce237a7ff6cc66c
ayush@DESKTOP-OCORL43 <ayush@DESKTOP-OCORL43.>
2025-02-04 22:04:02 +0000

    added restictions on x

--- mysqrt.py
+++ mysqrt.py
@@ -1,4 +1,9 @@
-def sqrt2(x, debug):
+def sqrt2(x, debug=False):
+    from numpy import nan
+    if x==0:
+        return 0
+    elif x<0:
+        return nan
     s= 1.5	
     kmax= 15
     tolerance= 1.0e-14;
